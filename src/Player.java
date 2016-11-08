a1062b7da326fcea774cf510f91744243b42ace2
Nick Espig <nickomator@gmail.com>
2016-11-08 10:57:44 +0100

    added Player class static variables DB_NAME and DB_LOCATION
    removed unused functions

---
+++ src/Player.java
@@ -0,0 +1,7 @@
+/**
+ * Created by nicko on 08.11.2016.
+ */
+public class Player {
+    private String name;
+    private int score;
+}
--- src/User_Highscore.java
+++ src/User_Highscore.java
@@ -11,17 +11,16 @@ import org.h2.tools.DeleteDbFiles;
 public class User_Highscore {
 
     private static final String DB_DRIVER = "org.h2.Driver";
-    private static final String DB_CONNECTION = "jdbc:h2:~/test";
+    private static final String DB_NAME = "test";
+    private static final String DB_LOCATION = "~/";
+    private static final String DB_CONNECTION = "jdbc:h2:" + DB_LOCATION + DB_NAME;
     private static final String DB_USER = "sa";
     private static final String DB_PASSWORD = "";
 
     public static void main(String[] args) throws Exception {
         try {
-            // delete the H2 database named 'test' in the user home directory
-            //DeleteDbFiles.execute("~", "test", true);
-            //insertWithStatement();
-            //DeleteDbFiles.execute("~", "test", true);
-            //insertWithPreparedStatement();
+            DeleteDbFiles.execute(DB_LOCATION, DB_NAME, true);
+            insertWithPreparedStatement();
             selectTopTenUser();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -65,34 +64,13 @@ public class User_Highscore {
         }
     }
 
-    // H2 SQL Statement Example
-    private static void insertWithStatement() throws SQLException {
-        Connection connection = getDBConnection();
-        Statement stmt = null;
-        try {
-            connection.setAutoCommit(false);
-            stmt = connection.createStatement();
-            stmt.execute("CREATE TABLE PERSON(id int primary key, name varchar(255))");
-
-            ResultSet rs = stmt.executeQuery("select * from PERSON");
-
-            stmt.close();
-            connection.commit();
-        } catch (SQLException e) {
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            connection.close();
-        }
-    }
     private static void selectTopTenUser() throws SQLException{
         Connection connection = getDBConnection();
         Statement stmt = null;
         try {
             connection.setAutoCommit(false);
             stmt = connection.createStatement();
-            ResultSet rs = stmt.executeQuery("select top 10 FROM Person order by score");
+            ResultSet rs = stmt.executeQuery("select * FROM Person order by score DESC LIMIT 10");
             System.out.println("H2 Database inserted through Statement");
             while (rs.next()) {
                 System.out.println("Id "+rs.getInt("id")+" Name "+rs.getString("name"));
